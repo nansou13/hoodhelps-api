5db982fcaf991d191f4526f7c4deb1875f705cfb
nansou13 <nans@gigan.fr>
2023-11-28 10:32:22 +0100

    send email on error

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const swaggerJSDoc = require('swagger-jsdoc')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const compression = require('compression')
+const nodemailer = require('nodemailer')
 
 const userRoutes = require('./routes/usersRoutes')
 const categoriesRoutes = require('./routes/categoriesRoutes')
@@ -50,6 +51,15 @@ const options = {
   apis: ['./routes/*.js'], // files containing annotations as above
 }
 
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ionos.fr',
+  port: 465,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+})
+
 const swaggerSpec = swaggerJSDoc(options)
 
 // Autorise toutes les origines à accéder à votre API (à des fins de développement)
@@ -59,10 +69,38 @@ app.use(compression())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.use('/api/users', userRoutes)
 app.use('/api/categories', categoriesRoutes)
 app.use('/api/groups', groupsRoutes)
+
+// Middleware pour gérer les erreurs
+app.use((err, req, res, next) => {
+  // Vérifiez si l'erreur est une erreur 4xx ou 5xx
+  if (err.status >= 400 && err.status < 600) {
+    // Format du message d'e-mail
+    const emailBody = `Erreur détectée: ${err.status} - ${err.message}\n\nStack Trace:\n${err.stack}`
+
+    // Envoyer l'e-mail
+    transporter.sendMail(
+      {
+        from: 'errorhoodhelps@gigan.fr',
+        to: 'hoodhelps@gigan.fr',
+        subject: "Erreur dans l'API",
+        text: emailBody,
+      },
+      (error) => {
+        if (error) {
+          console.log("Erreur lors de l'envoi de l'e-mail", error)
+        }
+      }
+    )
+  }
+
+  // Passez à l'erreur suivante (ou terminez la réponse si nécessaire)
+  next(err)
+})
 
 module.exports = app
